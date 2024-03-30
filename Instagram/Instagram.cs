e76f3cc64c4046bc2409896048b3127c45399422
Kevin Lu <LordTaitex@gmail.com>
2024-03-29 19:10:18 -0500

    Fixed if statements.

--- Instagram/Instagram.cs
+++ Instagram/Instagram.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +11,7 @@ using OpenQA.Selenium.Chrome;
 
 namespace InstagramWebScrape.Instagram;
 internal class Instagram
-{
+{ 
     private IWebDriver _driver;
     private int _img_counter = 1;
     public Instagram(string url)
@@ -45,31 +47,37 @@ internal class Instagram
 		}
     }
 
-    public void ImageSource()
+    public void SingleImagePost()
     {
         Thread.Sleep(5000);
 
-        var image = _driver.FindElement(By.XPath("//img[@class='x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3']"));
-        var src = image.GetAttribute("src");
+		// Single image post.
+		var img = _driver.FindElement(By.XPath("//img[@class='x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3']"));
+		var src = img.GetAttribute("src");
 
+		Console.WriteLine($"{_img_counter} : {src}");
+		_img_counter++;
+	}
 
-        Console.WriteLine($"{_img_counter} : {src}");
-        _img_counter++;
-    }
-
-    public void ReelSource()
+    public void MultiImagePost()
     {
         Thread.Sleep(5000);
 
-        var reel = _driver.FindElement(By.XPath(""));
-        var src = reel.GetAttribute("src");
-    }
+		// //li//img[@class='x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3 hoverZoomLink']  for more percise embedded images.
+		bool image = _driver.FindElement(By.XPath("//li//img[@class='x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3']")).Displayed;
 
-    /*
+        var img = _driver.FindElement(By.XPath("//li//img[@class='x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3']"));
+        var src = img.GetAttribute("src");
+
+		Console.WriteLine($"{_img_counter} : {src}");
+		_img_counter++;
+	}
+
+	/*
      Find the image and instead of looking immediately into the src of the image. Go find the Href and click on it. 
     From there, look at the image src and potentially more images if it is stacked post.
      */
-    public void FindImages()
+	public void FindImages()
     {
         Thread.Sleep(3000);
 
@@ -77,10 +85,18 @@ internal class Instagram
         var image = _driver.FindElements(By.XPath("//a[@class='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _a6hd']"));
 
         List<string> img_post = new();
-
-        List<string> img = new();
+        List<string> list_of_post_url = new();
 
         int count = 1;
+
+        foreach (var item in img_post)
+        {
+            Console.WriteLine("Stuff in img post");
+            Console.WriteLine(item);
+
+            Console.WriteLine("+++++++++++");
+        }
+
         foreach (var item in image)
         {
             
@@ -99,44 +115,71 @@ internal class Instagram
         {
             Console.WriteLine($"{count} : {item}");
 
-            img.Add(item);
+            list_of_post_url.Add(item);
 
             count++;
         }
+        Console.WriteLine("------------------");
 
-        
-        FindImgSrc(img);
+        FindImgSrc(list_of_post_url);
 
     }
 
     // Does the work of retreiving image src link.
     public void FindImgSrc(List<string> list_img)
     {
-        // Used to verify if the arrow button exists in the current post that driver is nagivating to.
-        //var arrow_btn_verification = _driver.FindElement(By.XPath("//button[@class=' _afxv _al46 _al47']"));
-        //var check_btn = _driver.FindElement(By.XPath("//button[@class=' _afxv _al46 _al47']")).Displayed;
+		// Used to verify if the arrow button exists in the current post that driver is nagivating to.
+		//var arrow_btn_verification = _driver.FindElement(By.XPath("//button[@class=' _afxv _al46 _al47']"));
+		//var check_btn = _driver.FindElement(By.XPath("//button[@class=' _afxv _al46 _al47']")).Displayed;
+
+		_driver.SwitchTo().NewWindow(WindowType.Tab);
 
 		foreach (var item in list_img)
         {
-            _driver.SwitchTo().NewWindow(WindowType.Tab);
-
             _driver.Navigate().GoToUrl(item);
-			// Find the img src and then check if arrow button exists.
+            // Find the img src and then check if arrow button exists.
 
-			// ** Need to fix the posts where it is a reel instead of an image.
-			// Video class is <video class="x1lliihq x5yr21d xh8yej3" />
-			// Need to also fix where is one post have multiple pictures, do not navigate to another post until all images are taken.
+            // Need to also fix where is one post have multiple pictures, do not navigate to another post until all images are taken.
             // In order to get reel url, just get selenium's tab url.
-			ImageSource();
-            ArrowButton();
 
-            Thread.Sleep(2000);
+            // If arrow exists, then it is a multi image post
+            // If mute button exists, then its a reel
+            // Else then it is a single post.
+
+            var video_reel = FindElementIfExists(_driver, By.XPath("//video[@class='x1lliihq x5yr21d xh8yej3']"));
+			
+            if (video_reel != null)
+			{
+                Console.WriteLine("-------");
+                Console.WriteLine(_driver.Url);
+				Console.WriteLine("-------");
+			}
+
+			var arrow_btn = FindElementIfExists(_driver, By.XPath("//div[@class=' _9zm2']"));
+
+            if (arrow_btn != null)
+            {
+                MultiImagePost();
+            }
+            else
+            {
+                SingleImagePost();
+            }
+
 		}
     }
 
+	// Checks if element exists or not. 
+	// Usage: var element = driver.FindElementIfExists(By.CssSelector("a[data-value*='09.0']"));
+	public IWebElement FindElementIfExists(IWebDriver _driver, By by)
+    {
+        var elements = _driver.FindElements(by);
+
+        return (elements.Count >= 1) ? elements.First() : null;
+    }
 
     // Located the arrow button on the image posts. If there is one, locate and click it until there is none signaling that it's at the end of the post.
-    public void ArrowButton()
+    public bool ArrowButton()
     {
         Thread.Sleep(5000);
 
@@ -147,13 +190,16 @@ internal class Instagram
             while (arrow_btn.Displayed)
             {
                 arrow_btn.Click();
-                ImageSource();
+                
             }
 
+            return true;
         }
         catch 
         {
             Console.WriteLine("Button dont exist.");
+
+            return false;
         }
 
     }
--- obj/Debug/net8.0/InstagramWebScrape.AssemblyInfo.cs
+++ obj/Debug/net8.0/InstagramWebScrape.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstagramWebScrape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85c75b9ceed19d39926e49b8a5cf976c999e6d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9abda793846ed93f53e2311f97e01e4e831ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstagramWebScrape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstagramWebScrape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
